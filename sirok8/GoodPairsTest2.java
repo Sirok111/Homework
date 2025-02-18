424532b02f8e915c214d620a8d17fabcd232aab9
Евген <artempetyh111@gmail.com>
2025-02-18 19:00:45 +0200

    Add files via upload

---
+++ sirok8/GoodPairsTest2.java
@@ -0,0 +1,29 @@
+package sirok8;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class GoodPairsTest2 {
+
+    @Test
+    public void testNumIdenticalPairs() {
+        // Тест 1: Приклад з вхідними даними {1, 2, 3, 1, 1, 3}
+        assertEquals(4, GoodPairs.numIdenticalPairs(new int[]{1, 2, 3, 1, 1, 3}));
+
+        // Тест 2: Всі елементи різні
+        assertEquals(0, GoodPairs.numIdenticalPairs(new int[]{1, 2, 3, 4}));
+
+        // Тест 3: Всі елементи однакові
+        assertEquals(6, GoodPairs.numIdenticalPairs(new int[]{1, 1, 1, 1}));
+
+        // Тест 4: Порожній масив
+        assertEquals(0, GoodPairs.numIdenticalPairs(new int[]{}));
+
+        // Тест 5: Один елемент
+        assertEquals(0, GoodPairs.numIdenticalPairs(new int[]{1}));
+
+        // Тест 6: Дублікати з різними індексами
+        assertEquals(1, GoodPairs.numIdenticalPairs(new int[]{1, 1}));
+    }
+}
+
